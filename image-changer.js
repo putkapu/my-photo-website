1c2c6c9a54196da114831421fab3db138ccc788a
Ruan Putka <ruan.putka@inv001027.involves.corp>
2021-05-12 17:05:35 -0300

    first commit

---
+++ image-changer.js
@@ -0,0 +1,24 @@
+// const fs = require('fs');
+// const length = fs.readdirSync('/home/directory').length;
+// console.log(length);
+
+var num_of_photos = 30;
+var backup_photos = Array.from({length: num_of_photos}, (x, i) => i+1);
+var photos =  [].concat(backup_photos);
+
+function removeElement(array, value) {
+	const index = array.indexOf(value);
+	array.splice(index, 1);
+};
+
+function changeImage() {
+	document.getElementById("1").src = ``;
+
+	if (photos.length == 0) {
+		photos = [].concat(backup_photos);
+	};
+
+	var n = photos[Math.floor(Math.random()*photos.length)];
+	removeElement(photos, n);
+	document.getElementById("1").src = `photos/${n}.jpg`;
+};
